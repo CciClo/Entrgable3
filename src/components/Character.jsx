d0780491129bf7756e66741d7c7173331bb2412e
Oscar Tandioy <tato.tandioy@gmail.com>
2022-11-06 20:41:36 -0500

    6/11/2022 8:41 pm

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 
-const Character = ({urlCharacter, selectedPage , index}) => {
+const Character = ({urlCharacter}) => {
 
   const [ character,setCharacter ] = useState({});
   
@@ -11,61 +11,48 @@ const Character = ({urlCharacter, selectedPage , index}) => {
       .then(obtainedCharacter => setCharacter(obtainedCharacter.data))
   }, []);
 
-  return (
-    <>
-    {  selectedPage == index &&
-      <div className={`character ${character.name}`}>
-
-        <img src={character.image} alt="Character image" />
-
-        <div className='features' >
-          <h3>
-            {character.name}
-          </h3>
-        </div>
-
-        <div className='features' >
-
-          <div className='status' >
-            <div className={ `${ character.status === "Alive"? 'green' : character.status==='Dead'? "red" : "gray" } circle` } ></div>
-            <h4>
-              {character.status}
-            </h4>
-          </div>
-
-          <p>
-            Specie:
-          </p>
-          <h4>
-            {character.species}
-          </h4>
-
-          <p>
-            Origin:
-          </p>
-          <h4>
-            {character.origin?.name}
-          </h4>
-
-          <p>
-            Type:
-          </p>
-          <h4>
-            {character.type}
-          </h4>
-
-          <p>
-            Number of episodes:
-          </p>
-          <h4>
-            {character.episode?.length}
-          </h4>
-
-        </div>
-
+  return (    
+    <div className={`character ${character.name}`}>
+      <img src={character.image} alt="Character image" />
+      <div className='features' >
+        <h3>
+          {character.name}
+        </h3>
       </div>
-    }
-    </>
+      <div className='features' >
+        <div className='status' >
+          <div className={ `${ character.status === "Alive"? 'green' : character.status==='Dead'? "red" : "gray" } circle` } ></div>
+          <h4>
+            {character.status}
+          </h4>
+        </div>
+        <p>
+          Specie:
+        </p>
+        <h4>
+          {character.species}
+        </h4>
+        <p>
+          Origin:
+        </p>
+        <h4>
+          {character.origin?.name}
+        </h4>
+        <p>
+          Type:
+        </p>
+        <h4>
+          {character.type}
+        </h4>
+        <p>
+          Number of episodes:
+        </p>
+        <h4>
+          {character.episode?.length}
+        </h4>
+      </div>
+    </div>
+    
   );
 };
 
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Character from './Character';
 
 const Page = ({ actualPage , index , selectedPage }) => {
-  console.log("pagina",index)
+  console.log(actualPage)
   return (
     <>
       { selectedPage == index &&
       <div className='character-container'>
-        {actualPage.map( urlCharacter => (<Character urlCharacter={urlCharacter} selectedPage={selectedPage} index={index} key={urlCharacter.name} />) )}
+        {actualPage.map( urlCharacter => (<Character urlCharacter={urlCharacter} selectedPage={selectedPage} index={index} key={urlCharacter} />) )}
       </div>
       }
     </>
